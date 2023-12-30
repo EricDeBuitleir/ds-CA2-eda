1234b3c9d7be159aa57c3dc66179802b5326c561
EricDeBuitleir <72872237+EricDeBuitleir@users.noreply.github.com>
2023-12-30 16:52:38 +0000

    Assignment 2: Process delete updated to allow for syntax using Delete instead of put, need to test to ensure it is functioning correctly

--- lambdas/processDelete.ts
+++ lambdas/processDelete.ts
@@ -2,18 +2,27 @@
 import { SQSHandler } from "aws-lambda";
 import * as dynamoDB from 'aws-cdk-lib/aws-dynamodb';
 import {
-  DeleteObjectCommand,
+  GetObjectCommand,
+  PutObjectCommandInput,
+  GetObjectCommandInput,
   S3Client,
-  
+  PutObjectCommand,
   
 } from "@aws-sdk/client-s3";
 
+// class similar to processImage.ts, functions commented out for image checking and syntax changed to allow for Delete Command
+
+// Reference for Deleteing objects: https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/dynamodb-example-dynamodb-utilities.html
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, PutCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
 
 
 
+const s3 = new S3Client();
 
+// Reference for consts below (Line 19 and 20): https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/dynamodb-example-dynamodb-utilities.html
+// const client = new DynamoDBClient({});
+// const docClient = DynamoDBDocumentClient.from(client);
 
 const dbClient = createDDbDocClient();
 
@@ -27,22 +36,30 @@ export const handler: SQSHandler = async (event) => {
       console.log("Record body ", JSON.stringify(snsMessage));
       for (const messageRecord of snsMessage.Records) {
         const s3e = messageRecord.s3;
-        const srcBucket = s3e.bucket.name;
+        // const srcBucket = s3e.bucket.name;
         // Object key may have spaces or unicode non-ASCII characters.
         const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
-        // Infer the image type from the file suffix.
-        const typeMatch = srcKey.match(/\.([^.]*)$/);
-        if (!typeMatch) {
-          console.log("Could not determine the image type.");
-          throw new Error("Could not determine the image type. ");
-        }
-        // Check that the image type is supported
-        const imageType = typeMatch[1].toLowerCase();
-        if (imageType != "jpeg" && imageType != "png") {
-          console.log(`Unsupported image type: ${imageType}`);
-          throw new Error("Unsupported image type: ${imageType. ");
-        }
+        // // Infer the image type from the file suffix.
+        // const typeMatch = srcKey.match(/\.([^.]*)$/);
+        // if (!typeMatch) {
+        //   console.log("Could not determine the image type.");
+        //   throw new Error("Could not determine the image type. ");
+        // }
+        // // Check that the image type is supported
+        // const imageType = typeMatch[1].toLowerCase();
+        // if (imageType != "jpeg" && imageType != "png") {
+        //   console.log(`Unsupported image type: ${imageType}`);
+        //   throw new Error("Unsupported image type: ${imageType. ");
+        // }
         // process image upload 
+        // reference to syntax for deleting an item into DynamoDB: https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/dynamodb-example-dynamodb-utilities.html
+        const deleteItem = new DeleteCommand({
+          TableName: "Img",
+          Key:{ // Key is used instead of item
+            "Name": srcKey,
+          },
+        });
+        await dbClient.send(deleteItem)
       } 
     
     }
